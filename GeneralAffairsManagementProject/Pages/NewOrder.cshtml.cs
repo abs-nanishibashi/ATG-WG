823c16d4f90101dfc11be931d071fa2eae06222b
BRAXTONJ\tmotoki <tmotoki@abeam.com>
2026-01-16 15:05:29 +0900

    庶務管理システム箱だけ作成コミット

---
+++ GeneralAffairsManagementProject/Pages/NewOrder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeneralAffairsManagementProject.Pages
+{
+    public class NewOrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
